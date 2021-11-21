287407dd6f26bf6a6ca72becda36c9687b0d7e0e
Kateryna Trofymenko <ekaterinatrofa@gmail.com>
2021-11-21 20:09:37 +0200

    up

--- main.cpp
+++ main.cpp
@@ -6,347 +6,314 @@
 #include <iterator>
 #include <algorithm>
 #include <math.h>
+#include <chrono>
+#include <experimental/random>
+#include <map>
+#include <stdlib.h>
+#include "subset_sum_problem.hpp"
+#include "hill_climbing.hpp"
+#include "tabu.hpp"
 
-using namespace std;
 
 
-using my_set = std::vector<double>;
-using my_subsets = std::vector<std::vector<double>>;
-
-double targetSum;
-
-struct Problem
+std::map<std::string, std::string> args_to_map(std::vector<std::string> arguments)
 {
-    my_set problem_ssp_set;
-    int sum;
-};
-
-ostream& operator<<(ostream& o, const my_set problem_ssp_set)
-{
-    o << "{";
-    for(int i = 0; i < problem_ssp_set.size(); i++)
+    std::map<std::string, std::string> ret;
+    std::string argname = "";
+    for (auto param : arguments)
     {
-        o << problem_ssp_set[i] << ",";
-    }
-    o << "}";
-    return o;
-}
-
-mt19937 rand_gen(time(nullptr));
-
-/**
-
-function for random generating set without duplicates numbers
-
-**/
-
-my_set generate_random_set(int n)
-{
-
-    uniform_int_distribution<int> distr(1, n*2);
-
-    my_set random_problem_set;
-
-    vector<double>::iterator it;
-
-    for (int i = 0; i < n; i++)
-    {
-
-        double x = distr(rand_gen);
-
-        it = find(random_problem_set.begin(),random_problem_set.end(),x);
-
-        if(it==random_problem_set.end())
+        if ((param.size() > 2) && (param.substr(0, 2) == "--"))
         {
-
-            random_problem_set.push_back(x);
+            argname = param.substr(2);
         }
         else
         {
-
-            i--;
+            ret[argname] = param;
         }
     }
-    return random_problem_set;
-}
-
-/**
-
-function that derives solution from randomly generated set
-working point
-
-**/
-
-
-my_set generate_random_subset (my_set random_set)
-{
-
-    my_set random_subset;
-
-    mt19937 rand_gen(time(nullptr));
-    uniform_int_distribution<int> distr(1, random_set.size()-1);
-
-    int x = distr(rand_gen);
-//    cout << x << endl;
-
-    std::sample(random_set.begin(), random_set.end(), std::back_inserter(random_subset),
-                x, std::mt19937{std::random_device{}()});
-
-    return random_subset;
-}
-
-Problem generate_random_problem(int n)
-{
-
-    my_set problem_set = generate_random_set(n);
-
-    my_set problem_sub_set = generate_random_subset(problem_set);
-
-    int temp_sum = std::accumulate(problem_sub_set.begin(),problem_sub_set.end(),0);
-
-    Problem my_problem;
-    my_problem.sum = temp_sum;
-    my_problem.problem_ssp_set = problem_set;
-
-
-    return my_problem;
-}
-
-Problem generate_problem_from_input(my_set input_set)
-{
-
-    my_set problem_sub_set = generate_random_subset(input_set);
-    int temp_sum = std::accumulate(problem_sub_set.begin(),problem_sub_set.end(),0);
-    Problem my_problem;
-    my_problem.sum = temp_sum;
-    my_problem.problem_ssp_set = input_set;
-
-
-    return my_problem;
+    return ret;
 }
 
 
-//vector<vector<double>> findAllSubsets(my_set ssp_problem)
-//{
-//
-//
-//    my_subsets subsets;
-//
-//
-//    for(int i = 0; i < (1 << ssp_problem.size()); i++)
-//    {
-//
-//        my_set current_subset;
-//
-//cout << " I" << i << endl;
-//
-//        for(int j = 0; j < ssp_problem.size(); j++)
-//        {
-//            if( (i & (1 << j)))
-//            {
-//                current_subset.push_back(ssp_problem[j]);
-//
-//            }
-//        }
-//        subsets.push_back(current_subset);
-//    }
-//
-//    return subsets;
-//}
+double targetSum;
 
-my_set findAllPossibleWorkPoints(my_set ssp_problem, int counter)
+
+auto next_point(my_set user_set, auto first, auto last)
 {
-
     my_set subset;
-
-    for (int i=0; i<ssp_problem.size(); i++)
+    for ( ; first != last  ; ++first)
     {
-
-        int index=1<<i;
-
-        if((index&counter)>0)
-        {
-
-            subset.push_back(ssp_problem[i]);
-
-        }
-
+        subset.push_back(user_set[(*first) - 1]);
     }
-
     return subset;
-}
+};
 
-
-double goal_function(my_set subsets, double targetSum)
+auto brute_force (my_set user_set,auto goal,int sum,std::ofstream &solFile)
 {
-    int temp_sum = std::accumulate(subsets.begin(),subsets.end(),0);
+    int stat_goal_function_calls = 0;
+    const int n = user_set.size();
+    std::vector<int> index_stack(n + 1, 0);
 
-    double difference = abs(temp_sum - targetSum);
+    int k = 0;
 
-    return difference;
-}
+    int best_goal = 0;
 
-//void printPowerset (my_set random_set)
-//{
-//    auto print_set = [&random_set](auto first, auto last) -> ostream&
-//    {
-//        cout << '(';
-//        auto sep = "";
-//        for ( ; first != last  ; ++first, sep = ",")
-//        {
-//            cout << sep << random_set[(*first) - 1];
-//        }
-//        return cout << ')';
-//    };
-//
-//    const int n = random_set.size();
-//    std::vector<int> index_stack(n + 1, 0);
-//    int k = 0;
-//
-//
-//    while(1){
-//        if (index_stack[k]<n){
-//            index_stack[k+1] = index_stack[k] + 1;
-//            k++;
-//        }
-//
-//        else{
-//            index_stack[k-1]++;
-//            k--;
-//        }
-//
-//        if (k==0)
-//            break;
-//
-//        print_set(begin(index_stack) + 1, begin(index_stack) + 1 + k);
-//    }
-//    print_set(begin(index_stack), begin(index_stack)) << endl;
-//}
+    my_set res;
+    std::cout << "Brute force" << std::endl;
+    solFile << "Brute force" << std::endl;
+    auto start = std::chrono::steady_clock::now();
 
+    auto best_p = generate_random_subset_size(user_set, std::experimental::randint(0, (int) (user_set.size()-1)));
 
-
-my_set load_problem(string fname)
-{
-    ifstream inputfile(fname);
-    my_set result;
-    std::string line;
-    while (std::getline(inputfile, line))
+    double best_goal_val = goal(best_p, sum);
+    //std::cout << "best" << best_goal_val << std::endl;
+    while(1)
     {
-
-        std::istringstream iss(line);
-        double value;
-        while (!iss.eof())
+        if (index_stack[k]<n)
         {
-            iss >> value;
-            if (value < 0) throw std::invalid_argument ("Invalid input data! All inputs should be positive ");
-            result.push_back(value);
+            index_stack[k+1] = index_stack[k] + 1;
+            k++;
         }
+
+        else
+        {
+            index_stack[k-1]++;
+            k--;
+        }
+
+        if (k==0)
+            break;
+
+        stat_goal_function_calls++;
+
+        res = next_point(user_set, begin(index_stack) + 1, begin(index_stack) + 1 + k);
+
+//        if (stat_goal_function_calls == std::experimental::randint(0, (int) (user_set.size()-1)/2))
+//        {
+//            best_goal_val = goal(res,sum);
+//        }
+        //if (stat_goal_function_calls == 1 ) best_goal_val = goal(res,sum);
+        double cost_value = goal(res,sum);
+
+        if (cost_value < best_goal_val)
+        {
+            best_goal_val = cost_value;
+
+            std::cout << res << " -> "  << "goal = " << best_goal_val << std::endl;
+            solFile   << res << " -> "  << best_goal_val << std::endl;
+
+
+        }
+
+
+//        if (goal(res,sum) == best_goal)
+//        {
+//            std::cout << res << " -> "  << best_goal << std::endl ;
+//            solFile   << res << " -> "  << best_goal << std::endl ;
+//        };
     }
-    return result;
+
+    auto finish = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> duration = finish - start;
+
+    solFile << "# count BF: " << stat_goal_function_calls << "; dt: " << duration.count() << std::endl;
+    std::cout << "# count BF: " << stat_goal_function_calls << "; dt: " << duration.count() << std::endl;
 }
 
-Problem read_problem(std::string filename)
+int main(int argc, char** argv)
 {
-    std::ifstream input_file(filename);
-    Problem out;
-    input_file >> out.sum;
-    while (!input_file.eof())
+
+
+    std::map<std::string, std::string> parameters =
     {
-        double val;
-        input_file >> val;
-        out.problem_ssp_set.push_back(val);
-    }
-    return out;
-}
+        {"size", "10"},
+        {"iterations","1000"},
+        {"method", "brute_force"},
+        {"print_result","false"},
+        {"tabu_size","100"},
+        {"inputfile",""},
+        {"outputfile",""}
+    };
 
-int main()
-{
+    for (auto [k, v] : args_to_map(std::vector<std::string>(argv, argv + argc)))
+    {
+        parameters[k] = v; // overwrite default parameters with the ones from CLI
+    }
+
+    int iterations = stoi(parameters["iterations"]);
 
     int choice;
 
-
-    cout << "How do you want data: "<< endl;
-    cout << endl;
-    cout << "1 -------> Read from file " << endl;
-    cout << "2 -------> Random generate" << endl;
-
-    cout << endl;
-    cout << "Enter 1 or 2: " << endl;
-
-    cin >> choice;
-
-
-    switch(choice)
+    if(!parameters["inputfile"].empty())
     {
-    case 1:
-    {
+        if(parameters["outputfile"].empty())
+        {
+            exit(EXIT_FAILURE);
+        }
 
-        cout << "Reading data from file" << endl;
 
-        //auto input = load_problem("ssp.txt");
-        auto problem = read_problem("ssp.txt");
+        std::cout << "Reading data from file : " << parameters["inputfile"] << std::endl;
 
-        // auto problem = generate_problem_from_input(pr);
-        cout << "Problem : " << endl;
-        cout << problem.problem_ssp_set << endl;
+        auto problem = read_problem(parameters["inputfile"]);
 
-        cout << "\nProblem SUM: " << problem.sum << endl;
+        std::ofstream solFile;
+        solFile.open(parameters["outputfile"]);
 
-        for(int i = 0; i < pow(2,problem.problem_ssp_set.size()); i++)
+        std::cout << "Problem : " << std::endl;
+        std::cout << problem.problem_ssp_set << std::endl;
+        std::cout << "Problem SUM: " << problem.sum << std::endl;
+        std::cout << "\nSolutions : " << std::endl;
+
+        if (parameters["method"] == "brute_force")
+        {
+            brute_force(problem.problem_ssp_set,&goal_function,problem.sum,solFile);
+        }
+        else if (parameters["method"] == "hill_climb")
+        {
+            hill_climb(problem.problem_ssp_set,&goal_function,problem.sum,iterations,solFile);
+        }
+        else if (parameters["method"] == "tabu")
         {
 
-            auto s_set = findAllPossibleWorkPoints(problem.problem_ssp_set,i);
-            auto solution = goal_function(s_set,problem.sum);
-            cout<< s_set;
+            tabu(problem.problem_ssp_set,&goal_function,problem.sum,iterations,stoi(parameters["tabu_size"]), solFile);
+        }
+
+        solFile.close();
+    }
+
+    else
+    {
+
+        std::cout << "How do you want data: "<< std::endl;
+        std::cout << std::endl;
+        std::cout << "1 -------> Read from file " << std::endl;
+        std::cout << "2 -------> Random generate" << std::endl;
+        std::cout << "3 -------> Input the problem to solve" << std::endl;
+
+        std::cout << std::endl;
+        std::cout << "Enter 1, 2 or 3: " << std::endl;
+
+        std::cin >> choice;
 
 
-            cout << " -> " <<solution << " ";
-            cout << endl;
+        switch(choice)
+        {
+        case 1:
+        {
+            std::string input_file;
+            std::string out_file;
+            std::cout << "Enter the file name with problem"<< std::endl;
+
+            std::cin >> input_file;
+
+            std::cout << "Reading data from file" << std::endl;
+
+            auto problem = read_problem(input_file);
+
+            std::cout << "Problem : " << std::endl;
+
+            std::cout << problem.problem_ssp_set << std::endl;
+
+            std::cout << "\nProblem SUM: " << problem.sum << std::endl;
+
+            std::cout << "Enter the name for file to save the results" << std::endl;
+
+            std::cin >> out_file;
+
+
+            std::cout << "\nSolutions : " << std::endl;
+
+
+            std::ofstream solFile;
+            solFile.open(out_file);
+
+            brute_force(problem.problem_ssp_set,&goal_function,problem.sum,solFile);
+            hill_climb(problem.problem_ssp_set,&goal_function,problem.sum,1000,solFile);
+            tabu(problem.problem_ssp_set,&goal_function,problem.sum,iterations,stoi(parameters["tabu_size"]), solFile);
+
+            solFile.close();
+
+            break;
+        }
+        case 2:
+        {
+
+            std::cout << "Generating random problem\n" << std::endl;
+
+            int n;
+            std::cout << "Please input the size of set\n" << std::endl;
+            std::cin >> n;
+            auto problem = generate_random_problem(n);
+
+
+            std::cout << "Problem : " << std::endl;
+            std::cout << problem.problem_ssp_set << std::endl;
+            std::cout << "\nProblem SUM: " << problem.sum << std::endl;
+            std::cout << "\nSolutions : " << std::endl;
+            std::ofstream null;
+
+//            auto cut = generate_random_subset_size(problem.problem_ssp_set,problem.problem_ssp_set.size()/2);
+//            std::cout << "cut " << cut << std::endl;
+//
+//            for(int i = 0; i<20; i++) {
+//                 auto d = find_close_neighbour(problem.problem_ssp_set, cut);
+//            std::cout << d << std::endl;
+//
+//            }
+
+            brute_force(problem.problem_ssp_set,&goal_function,problem.sum,null);
+            hill_climb(problem.problem_ssp_set,&goal_function,problem.sum,iterations,null);
+            tabu(problem.problem_ssp_set,&goal_function,problem.sum,iterations,stoi(parameters["tabu_size"]), null);
+
+            break;
+
+
+        }
+        case 3:
+        {
+            std::cin.clear();
+            std::cin.sync();
+
+            std::string set_num;
+            int user_sum;
+
+
+            std::cout << "Please input the set of numbers\n" << std::endl;
+
+            getline(std::cin, set_num);
+
+            std::stringstream input_parser_stream (set_num);
+            std::cout << std::endl;
+
+            std::cout << "Please input the target sum\n" << std::endl;
+            std::cin >> user_sum;
+
+
+
+            auto problem = generate_problem_from_input(input_parser_stream,user_sum);
+
+
+            std::cout << "Problem : " << std::endl;
+            std::cout << problem.problem_ssp_set << std::endl;
+            std::cout << "\nProblem SUM: " << problem.sum << std::endl;
+            std::cout << "\nSolutions : " << std::endl;
+            std::ofstream null;
+
+            brute_force(problem.problem_ssp_set,&goal_function,problem.sum,null);
+            hill_climb(problem.problem_ssp_set,&goal_function,problem.sum,pow(2, problem.problem_ssp_set.size()),null);
+            tabu(problem.problem_ssp_set,&goal_function,problem.sum,iterations,stoi(parameters["tabu_size"]), null);
+
+            break;
 
         }
 
-        break;
-    }
-    case 2:
-    {
-
-        cout << "Generating random problem\n" << endl;
-
-        int n;
-        cout << "Please input the size of set\n" << endl;
-        cin >> n;
-        auto problem = generate_random_problem(n);
-
-
-        cout << "Problem : " << endl;
-        cout << problem.problem_ssp_set << endl;
-        cout << "\nProblem SUM: " << problem.sum << endl;
-        cout << "\nSolutions : " << endl;
-
-
-        for(int i = 0; i < pow(2,problem.problem_ssp_set.size()); i++)
-        {
-
-            auto s_set = findAllPossibleWorkPoints(problem.problem_ssp_set,i);
-            auto solution = goal_function(s_set,problem.sum);
-            cout<< s_set;
-
-
-            cout << " -> " <<solution << " ";
-            cout << endl;
+        default:
+            std::cout << "Put correct choice" << std::endl;
+            break;
 
         }
-        break;
-
-
     }
-
-    default:
-        cout << "Put correct choice" << endl;
-        break;
-
-    }
-
 
 
     return 0;
